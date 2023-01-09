548f1da07d7875554c5724a0681b595ab863c3f4
Daniel Nunez <daniel.nunez@nav9.tech>
2023-01-09 10:52:32 -0300

    feat(React): add initial eslint configuration

---
+++ react.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+		
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'prettier/prettier': ["error", {
+      "trailingComma": "none",
+			"tabWidth": 2,
+			"semi": false,
+			"singleQuote": true,
+			"jsxSingleQuote": true,
+			"printWidth": 120,
+			"arrowParens": "avoid",
+			"bracketSpacing": true,
+			"jsxBracketSameLine": false
+    }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  }
+}
